4d8c08d90caa4d2a2c7712e9ee873d30f0ffeb38
Johann Monteiro <monteiro.johann@gmail.com>
2023-11-17 14:24:02 -0300

    Adding basic components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import 'tachyons';
-import Logo from './components/Logo/Logo';
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import "tachyons";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
 
 function App() {
+  //TSParticles Start
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+  //TSParticles End
+  
   return (
     <div className="App">
+      <Particles
+        id="tsparticles"
+        url="https://raw.githubusercontent.com/VincentGarreau/particles.js/master/demo/particles.json"
+        init={particlesInit}
+        loaded={particlesLoaded}
+      />
       <Navigation />
       <Logo />
+      <Rank />
+      <ImageLinkForm />
     </div>
   );
 }
